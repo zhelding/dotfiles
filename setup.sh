90f599a09db460a00ad78d9189381e77ccd6ea89
Zoe Helding <zoe.helding@gmail.com>
2019-05-07 13:48:45 -0500

    split setup file

--- setup.sh
+++ setup.sh
@@ -1,32 +1,6 @@
 #!/bin/sh
 
-# Base system setup
-
-cd ~
-sudo pacman -S git
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ~
-rm -rf yay
-
-git config --global user.email "zoe.helding@gmail.com"
-git config --global user.name "Zoe Helding"
-
-ln -s dotfiles/vimrc .vimrc
-yay -S vim vundle flake8 fzf tmux aws-cli powerline
-
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-
-yay -S ttf-dejavu nerd-fonts-dejavu-complete powerline-fonts
-
-ln -s dotfiles/zshrc .zshrc
-yay -S zsh zsh-theme-powerlevel9k
-chsh -s $(which zsh)
-
-yay -S ranger w3m
-
-yay -S pyenv python-pipenv
+source base-setup.sh
 
 # Base i3-gaps setup
 
